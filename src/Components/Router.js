4793ab0e23bebb39047a7e3aff99bd0d9393d5fd
dodong <qkrdkwl9090@naver.com>
2021-05-25 21:37:39 +0900

    add header styled Component

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,9 +2,11 @@ import React from "react";
 import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 import TV from "Routes/TV";
 export default () => (
     <Router>
+        <Header/>
         <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/tv" component={TV}/>
