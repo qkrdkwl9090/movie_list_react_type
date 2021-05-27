18c159bfbcd51d8bcf96ec5c144ab77b63ba227b
dodong <qkrdkwl9090@naver.com>
2021-05-27 22:19:39 +0900

    presenter 작업(하다맘)

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,23 @@
-export default () => "Search"
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+    movieResults,
+    tvResults,
+    error,
+    searchTerm,
+    handleSubmit,
+    loading
+}) => null;
+
+Searchresenter.propTypes = {    
+    movieResults:PropTypes.array,
+    tvResults:PropTypes.array,
+    error:PropTypes.bool.isRequired,
+    loading:PropTypes.string,
+    searchTerm:PropTypes.string,
+    handleSubmit: PropTypes.func.isRequired
+
+}
+export default TVPresenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,21 @@
-export default () => "TV"
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = ({
+    topRated,
+    popular,
+    airingToday,
+    error,
+    loading
+}) => null;
+
+TVPresenter.propTypes = {    
+    topRated:PropTypes.array,
+    popular:PropTypes.array,
+    airingToday:PropTypes.array,
+    error:PropTypes.bool.isRequired,
+    loading:PropTypes.string
+
+}
+export default TVPresenter;
