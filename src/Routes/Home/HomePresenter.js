5b04f1376ccf32f1b04e3038b85311caecef428b
Your Name <you@example.com>
2021-05-28 22:06:43 +0900

    add section

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+    padding:0px 10px;
+`;
 
 const HomePresenter = ({
     nowPlaying,
@@ -8,7 +13,11 @@ const HomePresenter = ({
     upcoming,
     error,
     loading
-}) => null;
+}) => loading ? null : (
+    <Container>
+        {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => movie.title)}</Section>}
+    </Container>
+);
 
 HomePresenter.propTypes = {    
     nowPlaying:PropTypes.array,
