19a564f69c45516543bfbf604177fa5d9236e190
Your Name <you@example.com>
2021-05-30 19:12:34 +0900

    revise Poster(title.length)

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -55,7 +55,7 @@ const Poster = ({id, imageUrl, title, rating, year, isMovie = false}) => (
                 {rating}/10
             </Rating>
         </ImageContainer>
-        <Title>{title}</Title>
+        <Title>{title.length > 18 ? `${title.substring(0, 18)}...` : title}</Title>
         <Year>{year}</Year>
     </Container>
 </Link>
